2bcfd7ee1860ae5a3b97d04a9e1b52f51700f71f
Phillip Berndt <phillip.berndt@googlemail.com>
2013-12-04 16:48:08 +0100

    Explaination for the remove_bug test

--- tests/remove_bug.c
+++ tests/remove_bug.c
@@ -1,3 +1,28 @@
+/*
+
+	This test was created to monitor a bug where upon deletion of a node the
+	replacer's left child tree vanished:
+
+	For example, consider
+
+	                 H
+	           D       -something-
+	      B      F
+	    A   C   E  G
+	       B'
+
+	with the bug, if D was removed, C would be the replacer element, because it
+	is the largest one in the left sub tree. C would take D's place and B would
+	be attached as C's left child. The bug is that this way, B' vanishes.
+
+	The fix is to attach B' as the right child of B, i.e. replace C with it's left
+	child. C can not have a right child, because then it would not be the largest
+	left child of D.
+
+	This test creates a big tree and then tries to remove every single node
+	from the full tree, each time checking the result for validity.
+
+*/
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
